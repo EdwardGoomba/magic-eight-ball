675f1589736d0f2f4a6050fa58baebeec2e8a38b
EdwardGoomba <ask@edwardvdesign.com>
2017-08-18 23:40:00 -0400

    Added case statements

--- js/magicEightBall.js
+++ js/magicEightBall.js
@@ -3,3 +3,38 @@ var userQuestion = '';
 var randomNumber = Math.floor(Math.random() * 7);
 
 var eightBall = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'You ate your last sandwhich';
+   	break;
+
+  case 1:
+    eightBall = 'More coffee';
+    break;
+
+  case 2:
+    eightBall = 'Go get me more chips!';
+    break;
+
+  case 3:
+    eightBall = 'Bro - you need to chill...';
+    break;
+
+  case 4:
+    eightBall = 'You are aware that I am not a genie right?';
+    break;
+
+  case 5:
+    eightBall = 'Nibbes and sips! Everyone wins with those...';
+    break;
+
+  case 6:
+    eightBall = 'Im starting to get tird of this';
+    break;
+
+  case 7:
+    eightBall = 'Kick it and jam some lofi hiphop';
+    break;
+
+                   }
